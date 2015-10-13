9b7e296697262eac85be2205df75e8f88d0c475c
Brian Olencki <bolencki13@gmail.com>
2015-10-13 18:50:09 -0400

    Fix Table not deselecting

---
+++ GoodNight/OpeningViewController.h
@@ -0,0 +1,13 @@
+//
+//  OpeningViewController.h
+//  GoodNight
+//
+//  Created by Brian Olencki on 10/13/15.
+//  Copyright © 2015 ADA Tech, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpeningViewController : UITableViewController
+
+@end
---
+++ GoodNight/OpeningViewController.m
@@ -0,0 +1,19 @@
+//
+//  OpeningViewController.m
+//  GoodNight
+//
+//  Created by Brian Olencki on 10/13/15.
+//  Copyright © 2015 ADA Tech, LLC. All rights reserved.
+//
+
+#import "OpeningViewController.h"
+
+@interface OpeningViewController ()
+
+@end
+
+@implementation OpeningViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+@end
