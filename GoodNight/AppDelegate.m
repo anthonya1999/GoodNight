7ed789592b5138acb45ee9918113dba39d4dcec0
Anthony Agatiello <anthonya1999@gmail.com>
2015-11-13 21:32:50 -0500

    Now only exits the app if you use the enable or disable action along with the URL scheme

--- GoodNight/AppDelegate.m
+++ GoodNight/AppDelegate.m
@@ -108,12 +108,15 @@
     if ([url.scheme isEqualToString: @"goodnight"]) {
         if ([url.host isEqualToString: @"enable"] && ![userDefaults boolForKey:@"enabled"]) {
             [GammaController enableOrangenessWithDefaults:YES transition:YES];
+            if ([[userDefaults objectForKey:@"keyEnabled"] isEqualToString:@"0"]) {
+                [GammaController suspendApp];
+            }
         }
         else if ([url.host isEqualToString: @"disable"] && [userDefaults boolForKey:@"enabled"]) {
             [GammaController disableOrangeness];
-        }
-        if ([[userDefaults objectForKey:@"keyEnabled"] isEqualToString:@"0"]) {
-            [GammaController suspendApp];
+            if ([[userDefaults objectForKey:@"keyEnabled"] isEqualToString:@"0"]) {
+                [GammaController suspendApp];
+            }
         }
     }
     return NO;
