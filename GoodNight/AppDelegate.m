ddcaf92a2e1ed09bc5286ebbf677d9a7453e7e4e
Mario Korte <mariokorte@users.noreply.github.com>
2015-11-30 19:06:21 +0100

    Optimize installing the BG task handler for more stable BG task execution

--- GoodNight/AppDelegate.m
+++ GoodNight/AppDelegate.m
@@ -51,6 +51,10 @@
     [self registerForNotifications];
     [AppDelegate updateNotifications];
     
+    if (application.applicationState == UIApplicationStateBackground) {
+        [self installBackgroundTask:application];
+    }
+    
     return YES;
 }
 
@@ -160,6 +164,31 @@
     }
 }
 
+- (BOOL) installBackgroundTask:(UIApplication *)application{
+    if (![userDefaults boolForKey:@"colorChangingEnabled"] && ![userDefaults boolForKey:@"colorChangingLocationEnabled"]) {
+        //If no auto option is enabled we do not need the keepAlive timer
+        [application clearKeepAliveTimeout];
+        [application setMinimumBackgroundFetchInterval:86400];
+        return NO;
+    }
+    
+    //Initiate the BG fetch timer and the keepAliveTimeout
+    [application setMinimumBackgroundFetchInterval:900];
+    
+    BOOL result = [app setKeepAliveTimeout:600 handler:^{
+        NSLog(@"KeepAliveTimeout Background Task start");
+        [userDefaults setObject:[NSDate date] forKey:@"lastBackgroundCheck"];
+        [userDefaults synchronize];
+        [GammaController autoChangeOrangenessIfNeededWithTransition:YES];
+        [NSThread sleepForTimeInterval:5.0];
+        NSLog(@"KeepAliveTimeout Background Task end");
+    }];
+    
+    NSLog(@"Installed background handler: %@", result?@"Success":@"Fail");
+    
+    return result;
+}
+
 - (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
     [GammaController autoChangeOrangenessIfNeededWithTransition:YES];
     [ForceTouchController exitIfKeyEnabled];
@@ -197,25 +226,7 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
-    if (![userDefaults boolForKey:@"colorChangingEnabled"] && ![userDefaults boolForKey:@"colorChangingLocationEnabled"]) {
-        //If no auto option is enabled we do not need the keepAlive timer
-        [application setMinimumBackgroundFetchInterval:86400];
-        return;
-    }
-    
-    //Initiate the BG fetch timer and the keepAliveTimeout
-    [application setMinimumBackgroundFetchInterval:900];
-    
-    BOOL result = [app setKeepAliveTimeout:600 handler:^{
-        NSLog(@"KeepAliveTimeout Background Task start");
-        [userDefaults setObject:[NSDate date] forKey:@"lastBackgroundCheck"];
-        [userDefaults synchronize];
-        [GammaController autoChangeOrangenessIfNeededWithTransition:YES];
-        [NSThread sleepForTimeInterval:5.0];
-        NSLog(@"KeepAliveTimeout Background Task end");
-    }];
-    
-    NSLog(@"Installed background handler: %@", result?@"Success":@"Fail");
+    [self installBackgroundTask:application];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -224,7 +235,7 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    [app clearKeepAliveTimeout];
+    [application clearKeepAliveTimeout];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
